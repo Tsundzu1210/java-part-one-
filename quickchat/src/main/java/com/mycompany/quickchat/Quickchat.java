f54500e8c1638804bab0a748338a07ab3bbf05ce
Tsundzu1210 <taniatsundzu@gmail.com>
2026-04-13 21:07:02 +0200

    Add files via upload

---
+++ quickchat/src/main/java/com/mycompany/quickchat/Quickchat.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quickchat;
+
+import java.util.Scanner;
+
+
+public class Quickchat {
+
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+        
+      
+       
+        System.out.print("Enter your firstname: ");
+        String firstname = scan.next();
+        
+        System.out.print("Enter your lastname: ");
+        String lastname = scan.next();
+        
+        System.out.print("Enter your username: ");
+        String username = scan.next();
+        
+        System.out.print("Enter your password: ");
+        String password = scan.next();
+        
+        System.out.print("Enter your cellphone: ");
+        String cellphone = scan.next();
+        
+        String result = Login.registerUser(firstname, lastname, username, password, cellphone);
+        System.out.println(result);
+        
+        if(result.contains("is not correctly")) {
+            return;
+        }
+        
+          System.out.print("Enter your username: ");
+        String loginUser = scan.next();
+        
+        System.out.print("Enter your password: ");
+        String loginPass = scan.next();
+        
+        boolean loginResult = Login.loginUser(loginUser, loginPass);
+        
+        System.out.println(Login.returnLoginStatus(loginResult));
+    }
+}
